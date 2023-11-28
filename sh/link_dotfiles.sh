5a23c768865e20f809f9322762a88fbf88ff1239
bfordAIMS <bford@aims.gov.au>
2023-11-28 21:09:35 +0800

    fix broken symlinks

--- sh/link_dotfiles.sh
+++ sh/link_dotfiles.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Note: ln (create link) -s (--symbolic) -f (--force)
+# Note: ln (create link) -s (--symbolic) -f (--force) -T (always treat link as file)
 
 # ====================
 #      Terminal 
 # ====================
-ln -sf ~/brendev-wsl/dotfiles/.bash_aliases      ~/.bash_aliases
-ln -sf ~/brendev-wsl/dotfiles/.bashrc            ~/.bashrc
-ln -sf ~/brendev-wsl/dotfiles/.zshrc             ~/.zshrc
-ln -sf ~/brendev-wsl/dotfiles/.zsh-theme         "$ZSH/themes/brendev.zsh-theme"
+ln -sf ~/.brendev-wsl/dotfiles/.bash_aliases      ~/.bash_aliases
+ln -sf ~/.brendev-wsl/dotfiles/.bashrc            ~/.bashrc 
+ln -sf ~/.brendev-wsl/dotfiles/.zshrc             ~/.zshrc
+ln -sf ~/.brendev-wsl/dotfiles/.zsh-theme         ~/.oh-my-zsh/themes/brendev.zsh-theme
 
 # ====================
 #         R 
 # ====================
-ln -sf ~/.dotfiles/dotfiles/.Rprofile                     ~/.Rprofile
+ln -sf ~/.brendev-wsl/dotfiles/.Rprofile          ~/.Rprofile
