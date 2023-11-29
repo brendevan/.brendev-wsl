ded21447e1f4e2be8649ed4c09f5be8815c1c5ec
bfordAIMS <bford@aims.gov.au>
2023-11-29 09:44:39 +0800

    git-filter-repo added to install.sh; sudo removed from pip3 install call (not tested)

--- sh/install.sh
+++ sh/install.sh
@@ -13,6 +13,13 @@ sudo apt update -y  && sudo apt install -y  \
 
 
 # ===================
+#        git
+# ===================
+# git-filter-repo
+pip3 install -U git-filter-repo
+
+
+# ===================
 #     oh-my-zsh
 # ===================
 # oh-my-zsh
@@ -31,10 +38,10 @@ sudo snap install codium --classic
 # ===================
 # R language
 yes | sudo bash $BDEV/sh/install_r.sh
-# radian console
-sudo pip3 install -U radian
-# jupyter  (used by quarto)
-sudo pip3 install -U jupyter
+# radian console                              ****REMOVED SUDO, NOT YET TESTED
+pip3 install -U radian
+# jupyter  (used by quarto)                   ****REMOVED SUDO, NOT YET TESTED
+pip3 install -U jupyter                       
 # quarto  (install with gdebi)
 # from: https://docs.posit.co/resources/install-quarto/#quarto-deb-file-install
 yes | sudo curl -LO https://quarto.org/download/latest/quarto-linux-amd64.deb
