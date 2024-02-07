0e213e707f39cb0e4a0213aac90ea031e79a2d95
bfordAIMS <bford@aims.gov.au>
2024-02-07 15:10:12 +0800

    minor

--- modules/r.sh
+++ modules/r.sh
@@ -43,4 +43,4 @@ done
 # =====================================
 msg-h1 "INSTALLING OTHER REQUIRED TOOLS\n"
 msg-h2 "Installing CmdStan (cmdstanr dependency)"
-R -e "cmdstanr::install_cmdstan()"
+sudo R -e "cmdstanr::install_cmdstan()"
